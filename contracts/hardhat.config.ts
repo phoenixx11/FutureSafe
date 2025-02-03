4c6b2d4895d36a4948d5712b5cb96eaf3b6d3c99
phoenix <140817661+phoenixx11@users.noreply.github.com>
2025-02-03 09:03:28 -0800

    Update hardhat.config.ts

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -8,8 +8,8 @@ const config: HardhatUserConfig = {
       url: "http://127.0.0.1:8545",
     },
     sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/kksljUTHDVAvILBoeYSu0qM2ybE2EFXD", // Replace with your Alchemy Project ID
-      accounts: ['0xbd11757a1dda972b7c67f1b1573a0a403f7b1ca8f119ffbf1d96aa7885b4910f'], // Replace with your private key
+      url: "https://eth-sepolia.g.alchemy.com/v2/", // Replace with your Alchemy Project ID
+      accounts: [''], // Replace with your private key
     },
   },
   solidity: "0.8.24",
